2721034ed43d5a7bdfab31721583f32f2d2c7db9
WillieSoFairy <williesu123456@outlook.com>
2024-06-11 00:18:04 +0800

    refactor(DAO): switch to mysql instead of tcb_db

--- app/functions/add_ent/index.js
+++ app/functions/add_ent/index.js
@@ -1,14 +1,18 @@
-const tcb = require("@cloudbase/node-sdk");
-const app = tcb.init({
-    'env': process.env.ENV_ID
-});
-const db = app.database();
+const dbConfig = require("./db.config");
+const mysql = require('mysql2/promise').createPool(dbConfig);
 
 exports.main = async (event, context) => {
-    const db_res = await db.collection("enterprise")
-        .add({
-            "ent_name": event.ent_name,
-            "enable": true
-        });
-    return db_res;
+    const ent_name = event.ent_name;
+    const sql = `INSERT INTO
+    enterprise (create_time, ent_name, \`enable\`)
+VALUES
+    (NOW(), '${ent_name}', TRUE);`
+    try {
+        const [results] = await mysql.query(sql);
+        return results.insertId;
+    }
+    catch (err) {
+        console.log(err);
+        return -1;
+    }
 }
--- app/functions/add_unit/index.js
+++ app/functions/add_unit/index.js
@@ -1,13 +1,18 @@
-const tcb = require("@cloudbase/node-sdk");
-const app = tcb.init({
-    'env': process.env.ENV_ID
-});
-const db = app.database();
+const dbConfig = require("./db.config");
+const mysql = require('mysql2/promise').createPool(dbConfig);
 
 exports.main = async (event, context) => {
-    const db_res = await db.collection("unit")
-        .add({
-            "unit_name": event.unit_name
-        });
-    return db_res;
+    const unit_name = event.unit_name;
+    const sql = `INSERT INTO
+    unit (create_time, unit_name)
+VALUES
+    (NOW(), '${unit_name}');`
+    try {
+        const [results] = await mysql.query(sql);
+        return results.insertId;
+    }
+    catch (err) {
+        console.log(err);
+        return -1;
+    }
 }
