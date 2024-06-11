8393a3f2e46ee4cda35f3486878f37bba8621e43
WillieSoFairy <williesu123456@outlook.com>
2024-06-11 23:01:01 +0800

    feat(View): add new order records by UI

--- cc-vue/src/Pages/FormTable.vue
+++ cc-vue/src/Pages/FormTable.vue
@@ -1,15 +1,18 @@
 <template>
-    <a-row><a-form><a-form-item label="选择日期"><a-date-picker /></a-form-item></a-form></a-row>
+    <a-row><a-form><a-form-item label="选择日期"><a-date-picker v-model:value="order_date" format="YYYY/MM/DD"
+                    @change="getOrderData" /></a-form-item></a-form></a-row>
     <a-row>
         <h1>1/5</h1>
     </a-row>
     <a-row>
-        <a-col :span="10"><a-image :src=testImg width="32vw"/></a-col>
+        <a-col :span="10"><a-image :src=testImg width="32vw" /></a-col>
         <a-col :span="14">
             <a-form>
-                <a-form-item label="单位名称" style="width: 30em;"><a-input /></a-form-item>
+                <a-form-item label="单位名称" style="width: 30em;"><a-input-search v-model:value="ent_name" enter-button
+                        @search="getOrderData" :loading="search_loading" /></a-form-item>
             </a-form>
-            <DataTable />
+            <a-button type="primary" @click="handleAddItem">新增项目</a-button>
+            <DataTable v-model="orderData" />
         </a-col>
     </a-row>
     <a-row style="margin-top: 30px;">
@@ -22,6 +25,42 @@
     </a-row>
 </template>
 <script setup>
-import DataTable from '../components/DataTable.vue'
-import testImg from '../images/1.jpg'
+import { ref } from 'vue';
+import dayjs from 'dayjs';
+import DataTable from '../components/DataTable.vue';
+import { auth } from '../tcb/index.js';
+import testImg from '../images/1.jpg';
+import { get_orderData } from '../components/FormQueryOrder';
+
+const now = dayjs(new Date());
+const orderData = ref(null);
+const order_date = ref(now);
+const ent_name = ref(null)
+
+async function handleAddItem() {
+    let newKey = null;
+    if (orderData.value.length === 0) {
+        newKey = 0;
+    }
+    else if (orderData.value.at(-1).id !== null) {
+        newKey = orderData.value.at(-1).key + 1;
+    }
+    else { return; }
+    const { customUserId } = await auth.getCurrenUser();
+    orderData.value.push({
+        "key": newKey,
+        "id": null,
+        "ent_name": ent_name.value,
+        "user_id": customUserId,
+        "order_date": order_date.value,
+        "editable": true
+    });
+}
+
+const search_loading = ref(false);
+async function getOrderData() {
+    search_loading.value = true;
+    orderData.value = await get_orderData(order_date.value, ent_name.value);
+    search_loading.value = false;
+}
 </script>
--- cc-vue/src/components/DataTable.vue
+++ cc-vue/src/components/DataTable.vue
@@ -1,82 +1,122 @@
 <template>
-    <a-table :columns="columns" :data-source="dataSource" bordered>
+    <a-table :columns="columns" :data-source="orderData" bordered :pagination="false">
         <template #bodyCell="{ column, text, record }">
-            <template v-if="['name', 'quantity', 'unit', 'dept'].includes(column.dataIndex)">
-                <div>
-                    <a-input v-if="editableData[record.key]" v-model:value="editableData[record.key][column.dataIndex]"
-                        style="margin: -5px 0" />
-                    <template v-else>
-                        {{ text }}
-                    </template>
-                </div>
+            <template v-if="column.dataIndex === 'good_name'">
+                <template v-if="record.editable === false">{{ text }}</template>
+                <a-input v-else v-model:value="uploadData.good_name" />
             </template>
-            <template v-else-if="column.dataIndex === 'operation'">
-                <div class="editable-row-operations">
-                    <span v-if="editableData[record.key]">
-                        <a-typography-link @click="save(record.key)">保存</a-typography-link>
-                        <a @click="cancel(record.key)">取消</a>
-                    </span>
-                    <span v-else>
-                        <a @click="edit(record.key)">修改</a>
-                        <a>删除</a>
-                    </span>
-                </div>
+            <template v-if="column.dataIndex === 'count'">
+                <template v-if="record.editable === false">{{ text }}</template>
+                <a-input v-else v-model:value="uploadData.count" />
+            </template>
+            <template v-if="column.dataIndex === 'unit_name'">
+                <template v-if="record.editable === false">{{ text }}</template>
+                <a-input v-else v-model:value="uploadData.unit_name" />
+            </template>
+            <template v-if="column.dataIndex === 'dept_name'">
+                <template v-if="record.editable === false">{{ text }}</template>
+                <a-input v-else v-model:value="uploadData.dept_name" />
+            </template>
+            <template v-if="column.key === 'operation'">
+                <span v-if="record.editable === false">
+                    <a-typography-link @click="handleEdit(record.key)">修改</a-typography-link>
+                    <a-divider type="vertical" />
+                    <a-typography-link type="danger">删除</a-typography-link>
+                </span>
+                <span v-else>
+                    <a-typography-link type="success" v-if="record.id === null"
+                        @click="handleAdd(record.key)">保存</a-typography-link>
+                    <a-typography-link type="success" v-else>保存</a-typography-link>
+                    <a-divider type="vertical" />
+                    <a-typography-link @click="handleCancel(record.key)">取消</a-typography-link>
+                </span>
             </template>
         </template>
     </a-table>
 </template>
 <script setup>
 import { cloneDeep } from 'lodash-es';
-import { reactive, ref } from 'vue';
+import { ref } from 'vue';
+import { get_orderData, add_orderData } from '../components/FormQueryOrder';
+
+const orderData = defineModel();
 const columns = [
     {
         title: '名称',
-        dataIndex: 'name',
+        dataIndex: 'good_name',
         width: '25%',
     },
     {
-        title: '单位',
-        dataIndex: 'unit',
-        width: '10%',
+        title: '数量',
+        dataIndex: 'count',
+        width: '10%'
     },
     {
-        title: '数量',
-        dataIndex: 'quantity',
+        title: '单位',
+        dataIndex: 'unit_name',
         width: '10%',
     },
     {
         title: '部门',
-        dataIndex: 'dept',
+        dataIndex: 'dept_name',
         width: '20%',
     },
     {
         title: '操作',
-        dataIndex: 'operation',
+        key: 'operation',
     },
 ];
-const data = [];
-for (let i = 0; i < 100; i++) {
-    data.push({
-        key: i.toString(),
-        name: `生菜 ${i + 1}`,
-        unit: 'kg',
-        quantity: 1,
-        dept: '津津员工'
-    });
+
+
+const uploadData = ref(resetInput());
+
+async function handleAdd(key) {
+    const keyItem = { ...orderData.value[key] };
+    uploadData.value.ent_name = keyItem.ent_name;
+    uploadData.value.user_id = keyItem.user_id;
+    uploadData.value.order_date = keyItem.order_date;
+
+    await add_orderData(uploadData.value);
+    orderData.value = await get_orderData(keyItem.order_date, keyItem.ent_name);
+
+    uploadData.value = resetInput();
 }
-const dataSource = ref(data);
-const editableData = reactive({});
-const edit = key => {
-    editableData[key] = cloneDeep(dataSource.value.filter(item => key === item.key)[0]);
-};
-const save = key => {
-    Object.assign(dataSource.value.filter(item => key === item.key)[0], editableData[key]);
-    delete editableData[key];
-};
-const cancel = key => {
-    delete editableData[key];
-};
+
+function handleCancel(key) {
+    if (orderData.value[key].id === null) {
+        orderData.value.pop();
+    }
+    else {
+        orderData.value[key].editable = false;
+    }
+    uploadData.value = resetInput();
+}
+
+function handleEdit(key) {
+    const keyItem = { ...orderData.value[key] };
+    uploadData.value = {
+        "dept_name": keyItem.dept_name,
+        "good_name": keyItem.good_name,
+        "unit_name": keyItem.unit_name,
+        "count": keyItem.count,
+    };
+    orderData.value[key].editable = true;
+}
+
+function resetInput() {
+    return {
+        "ent_name": null,
+        "dept_name": null,
+        "good_name": null,
+        "unit_name": null,
+        "count": null,
+        "user_id": null,
+        "order_date": null
+    };
+}
 </script>
+
+
 <style scoped>
 .editable-row-operations a {
     margin-right: 8px;
