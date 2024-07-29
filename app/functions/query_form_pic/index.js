998a7eb0a4f808ea417920662bdd585d5d078f68
WillieSoFairy <williesu123456@outlook.com>
2024-07-29 21:26:37 +0800

    feat: query temp picture URL

--- app/functions/query_form_pic/index.js
+++ app/functions/query_form_pic/index.js
@@ -1,5 +1,10 @@
+const tcb = require("@cloudbase/node-sdk");
+const app = tcb.init({ 'env': process.env.ENV_ID });
+
 const dbConfig = require("./db.config");
 const mysql = require('mysql2/promise').createPool(dbConfig);
+
+const maxAge = 5 * 60 * 1000;
 
 exports.main = async (event, context) => {
     const order_date = event.order_date;
@@ -8,8 +13,9 @@ exports.main = async (event, context) => {
     try {
         const total = await query_pages(order_date);
         if (page > total) { throw "Pages overflow"; }
-        const details = await get_pic_detail(order_date, page, total);
-        return details;
+        const details = await get_pic_detail(order_date, page);
+        const pic_url = await get_pic_url(details.fileID);
+        return { pic_url: pic_url, ent_id: details.ent_id, pageNum: page, total: total }
     }
     catch (err) { return { error: err }; }
 }
@@ -23,11 +29,18 @@ async function query_pages(order_date) {
     catch { throw "Query pages error"; }
 }
 
-async function get_pic_detail(order_date, pageNum, total) {
+async function get_pic_detail(order_date, pageNum) {
     const sql = `SELECT * FROM order_pictures WHERE order_date='${order_date}' ORDER BY id LIMIT 1 OFFSET ${pageNum - 1};`;
     try {
         const [result] = await mysql.query(sql);
-        return { path: result[0].path, ent_id: result[0].ent_id, total: total, pageNum: pageNum };
+        return { fileID: result[0].path, ent_id: result[0].ent_id };
     }
     catch { throw "Query detail error"; }
+}
+
+async function get_pic_url(fileID) {
+    const result = await app.getTempFileURL({
+        fileList: [{ fileID: fileID, maxAge: maxAge }]
+    });
+    return result.fileList[0].tempFileURL;
 }
