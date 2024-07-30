76270932f66ad58e8e0a6cd8d32e939dfabff6e4
WillieSoFairy <williesu123456@outlook.com>
2024-07-30 23:36:21 +0800

    feat: add "ent_name", thumb when uploading peatures

--- cc-vue/src/components/FormPics.js
+++ cc-vue/src/components/FormPics.js
@@ -11,27 +11,22 @@ async function get_pic(order_date, pageNum) {
             }
         });
         const { result } = res;
-        // console.log(result)
         if (result.error !== null) { throw result.error; }
         return { pic_url: result.pic_url, ent_id: result.ent_id, pageNum: result.pageNum, total: result.total }
     }
     catch (err) { throw err; }
 }
 
-async function upload_pics(pics, order_date) {
+async function upload_pics(pics, order_date, ent_name) {
     const user_id = await auth.getCurrenUser();
-    console.log({
-        pics: pics,
-        order_date: order_date,
-        user_id: user_id
-    })
     try {
         const res = await tcb.callFunction({
             name: 'upload_pictures',
             data: {
                 pics: pics,
                 order_date: dayjs(order_date).format('YYYY-MM-DD'),
-                user_id: user_id.customUserId
+                user_id: user_id.customUserId,
+                ent_name: ent_name
             }
         });
         return res.result;
