37f2faba4764145e9b5935530489804a46524bb9
WillieSoFairy <williesu123456@outlook.com>
2024-08-03 01:12:41 +0800

    feat: use new logic to create order record and change parameterized query to make query safer

--- app/functions/add_order/index.js
+++ app/functions/add_order/index.js
@@ -1,14 +1,13 @@
 const tcb = require("@cloudbase/node-sdk");
-const app = tcb.init({
-    'env': process.env.ENV_ID
-});
+const app = tcb.init({ 'env': process.env.ENV_ID });
 
 const dbConfig = require("./db.config");
 const mysql = require('mysql2/promise').createPool(dbConfig);
 
 exports.main = async (event, context) => {
     //enterprise
-    const ent_id = await get_entID(event.ent_name);
+    // const ent_id = await get_entID(event.ent_name);
+    const ent_id = event.ent_id;
 
     //department
     const dept_id = await get_deptID(event.dept_name, ent_id);
@@ -21,15 +20,17 @@ exports.main = async (event, context) => {
 
     const count = event.count;
     const user_id = event.user_id;
-    const order_date = event.order_date;
+    // const order_date = event.order_date;
+    const pic_id = event.pic_id;
 
     const sql = `INSERT INTO
-order_details (create_time,ent_id,dept_id,good_id,unit_id,count,user_id,order_date)
+order_details (create_time,pic_id,dept_id,good_id,unit_id,count,user_id)
 VALUES
-(NOW(), ${ent_id}, ${dept_id}, ${good_id}, ${unit_id}, ${count}, '${user_id}', '${order_date}');`;
+(NOW(), ?, ?, ?, ?, ?, ?);`;
 
     try {
-        const [results] = await mysql.query(sql);
+        const [results] = await mysql.query(sql, [pic_id, dept_id, good_id, unit_id, count, user_id]);
+        await update_pic_ent(pic_id, ent_id);
         return results.insertId;
     }
     catch (err) {
@@ -39,28 +40,28 @@ VALUES
 }
 
 async function check_exist(table, field, data) {
-    const sql = `select id from ${table} WHERE ${field}='${data}';`;
-    const [results] = await mysql.query(sql);
+    const sql = `select id from ${table} WHERE ${field}=?;`;
+    const [results] = await mysql.query(sql, [data]);
     return results;
 }
 
-async function get_entID(ent_name) {
-    const db_res = await check_exist("enterprise", "ent_name", ent_name);
-    if (db_res.length !== 0) {
-        return (db_res[0]).id;
-    }
-    else {
-        const fn_res = await app.callFunction({
-            name: "add_ent",
-            data: { "ent_name": ent_name }
-        });
-        return fn_res.result;
-    }
-}
+// async function get_entID(ent_name) {
+//     const db_res = await check_exist("enterprise", "ent_name", ent_name);
+//     if (db_res.length !== 0) {
+//         return (db_res[0]).id;
+//     }
+//     else {
+//         const fn_res = await app.callFunction({
+//             name: "add_ent",
+//             data: { "ent_name": ent_name }
+//         });
+//         return fn_res.result;
+//     }
+// }
 
 async function get_deptID(dept_name, ent_id) {
-    const sql = `select id from department WHERE dept_name='${dept_name}' and ent_id='${ent_id}';`
-    const [db_res] = await mysql.query(sql);
+    const sql = `select id from department WHERE dept_name=? and ent_id=?;`
+    const [db_res] = await mysql.query(sql, [dept_name, ent_id]);
     if (db_res.length !== 0) {
         return (db_res[0]).id;
     }
