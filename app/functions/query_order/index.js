6203ecd369f0b29bb52f1c98d4090e42e0e61d82
WillieSoFairy <williesu123456@outlook.com>
2024-08-03 01:13:39 +0800

    feat: use new logic to select order record and change parameterized query to make query safer

--- app/functions/query_order/index.js
+++ app/functions/query_order/index.js
@@ -2,11 +2,10 @@ const dbConfig = require("./db.config");
 const mysql = require('mysql2/promise').createPool(dbConfig);
 
 exports.main = async (event, context) => {
-    const order_date = event.order_date;
-    const ent_name = event.ent_name;
+    const pic_id = event.pic_id;
     try {
-        const sql = `SELECT id,ent_name,dept_name,good_name,count,unit_name,order_date from order_view where order_date='${order_date}' and ent_name='${ent_name}';`;
-        const [results] = await mysql.query(sql);
+        const sql = `SELECT id,ent_name,dept_name,good_name,count,unit_name,order_date,pic_id from order_view2 where pic_id=?;`;
+        const [results] = await mysql.query(sql, [pic_id]);
         return results;
     } catch {
         return null;
